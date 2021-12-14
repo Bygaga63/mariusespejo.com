529eab53edaf0d10a95e4ede61d592d323f1dde6
mariusespejo <5385443+mariusespejo@users.noreply.github.com>
2021-12-13 23:36:30 -0800

    update base meta

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -49,8 +49,8 @@ export let loader: LoaderFunction = () => {
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
+    title: "Marius Espejo",
+    description: "Hello!"
   };
 };
 
