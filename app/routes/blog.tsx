84cea70dc688a8568f139dbd001b977433c06f92
mariusespejo <5385443+mariusespejo@users.noreply.github.com>
2022-01-16 13:22:18 -0800

    add tailwind config, plus typography plugin

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,10 +1,10 @@
 import styles from 'highlight.js/styles/github-dark-dimmed.css';
 import type { LinksFunction, LoaderFunction } from 'remix';
-import { MetaFunction, Outlet } from "remix";
+import { MetaFunction, Outlet } from 'remix';
 
 export let loader: LoaderFunction = (whatthis) => {
   return null;
-}
+};
 
 export const links: LinksFunction = () => {
   return [
@@ -13,17 +13,21 @@ export const links: LinksFunction = () => {
       href: styles,
     },
   ];
-}
+};
 
 export default function Blog() {
   return (
-    <Outlet />
+    <div className="flex justify-center">
+      <div className="prose lg:prose-xl py-10">
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
 export let meta: MetaFunction = () => {
   return {
-    title: "Blog | Marius Espejo",
-    description: "Blog posts by Marius Espejo"
+    title: 'Blog | Marius Espejo',
+    description: 'Blog posts by Marius Espejo',
   };
 };
